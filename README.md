99b8dc1fe8da4171e8360eb9d063f6b57cb016c5
TuurvdHave <47116389+TuurvdHave@users.noreply.github.com>
2022-12-08 11:31:55 +0100

    Adding the Kalman smoother
    Add the kalman smoother to the pipeline (Dan will test it)

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Step 2: Do all the different steps in OpenSim and save your scaled model in the 
 			- Externalloads file: ExternalLoads.xml<br>
 			- SO setup: SO_generic.xml<br>
 			- Joint contact forces analyses: JRA_generic.xml<br>
-Step 3: Adapt the params.json file with a text editor. The "osim_path" should refer to intallation folder of your OpenSim. The other lines should be adapted to the information of your project.<br> 
+Step 3: Adapt the params.json file with a text editor. The "osim_path" should refer to intallation folder of your OpenSim. The ks3x or the ks4x should refer to the folder containing the kalman smoother (kalman smoother can be found here: https://simtk.org/projects/kalmanforik)
+make sure you have the right KS to your OpenSim version. The other lines should be adapted to the information of your project.<br> 
 Step 4: run the matlab code Run_all.m <br>
 Step 5: Install the reporting app by double clicking on the Patients Display (Matlab App Installer). After installation, you should be able to see the app in matlab in the tab "app"<br>
 Step 6: Run the reporting app and import your own data. <br>
--- Run_All.m
+++ Run_All.m
@@ -12,7 +12,7 @@ for i = 3 : size(folders,1)
     end 
 end 
 [file,path] = uigetfile('*.*','Select params.json file to define specific inputs');
-answer = inputdlg({'IK? Answer with yes or no','ID?','SO?','JRF?','Save in .mat?'},'Analyses',[1 35],{'yes','yes','yes','yes','yes'});
+answer = inputdlg({'IK? Answer with yes or no','KS?','ID?','SO?','JRF?','Save in .mat?'},'Analyses',[1 35],{'yes','yes','yes','yes','yes','yes'});
 %% Running the analysis 
 for subjectnr = 1 : size(subjectname,2)
     filenames = dir(char(fullfile(path,subjectname(subjectnr))));
@@ -22,15 +22,18 @@ for subjectnr = 1 : size(subjectname,2)
             IK(fullfile(path,file),filenames(nfile).name,subjectname(subjectnr),mainpath);
             end 
             if strcmpi(answer(2,1),'yes')
-            ID(fullfile(path,file),[filenames(nfile).name(1:end-4) '.mot'],subjectname(subjectnr),mainpath);
+            KS(fullfile(path,file),filenames(nfile).name,subjectname(subjectnr),mainpath);
             end
             if strcmpi(answer(3,1),'yes')
-            SO(fullfile(path,file),[filenames(nfile).name(1:end-4) '_ExternalLoads.xml'],subjectname(subjectnr),mainpath);
+            ID(fullfile(path,file),[filenames(nfile).name(1:end-4) '.mot'],subjectname(subjectnr),mainpath);
             end
             if strcmpi(answer(4,1),'yes')
-            JRF(fullfile(path,file),[filenames(nfile).name(1:end-4) '_StaticOptimization_force.sto'],subjectname(subjectnr),mainpath);
+            SO(fullfile(path,file),[filenames(nfile).name(1:end-4) '_ExternalLoads.xml'],subjectname(subjectnr),mainpath);
             end
             if strcmpi(answer(5,1),'yes')
+            JRF(fullfile(path,file),[filenames(nfile).name(1:end-4) '_StaticOptimization_force.sto'],subjectname(subjectnr),mainpath);
+            end
+            if strcmpi(answer(6,1),'yes')
             Summarize(fullfile(path,file),[filenames(nfile).name(1:end-4) '_JointReaction_ReactionLoads.sto'],subjectname(subjectnr),mainpath);
             end 
         end 
